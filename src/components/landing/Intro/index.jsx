dd7ea1a0bec835aeba6cd3e103ff7105c271bde8
Jakom <48088579+RyouYoo@users.noreply.github.com>
2021-04-26 21:10:46 +0000

    Update index.jsx

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -15,13 +15,13 @@ export const Intro = () => {
       <IntroWrapper as={Container}>
         <Details theme={theme}>
           <h1>Hi There!</h1>
-          <h4>I’m John and I’m a JAMStack engineer!</h4>
+          <h4>I’m Jakom and I’m a Back-End Developer and Penetration Tester!</h4>
           <Button as={AnchorLink} href="#contact">
             Hire me
           </Button>
         </Details>
         <Thumbnail>
-          <img src={dev} alt="I’m John and I’m a JAMStack engineer!" />
+          <img src={dev} alt="I’m Jakom and I’m a Back-End Developer and Penetration Tester!" />
         </Thumbnail>
       </IntroWrapper>
     </Wrapper>
